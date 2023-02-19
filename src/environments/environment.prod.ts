adc4e328508e3b1b741d20deb47076a7ea8c8355
Stanislav_Polivoda <stanislav_polivoda@epam.com>
2023-02-19 01:04:18 +0100

    upd d

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/prod',
+    product: 'https://1owphsxn20.execute-api.us-east-1.amazonaws.com/prod',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
